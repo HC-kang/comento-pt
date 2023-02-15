24594f5d46c121f90347111aba162deb2235c113
Ford10082 <81678439+HC-kang@users.noreply.github.com>
2023-02-15 17:07:21 +0900

    Chore: test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! OIDC';
+    return 'Hello World! OIDC2';
   }
 }
